e5bfafb5f52b72d40f6f5aba03413403bb7246c4
Andrey Karamnoff <karamnoff@gmail.com>
2021-01-08 16:19:40 +0300

    Complete header

--- markup/components/header/data/data.js
+++ markup/components/header/data/data.js
@@ -1,2 +1,25 @@
 /* eslint-disable no-unused-vars */
-const data = { header: {} };
+const data = {
+  header: {
+    socials: [
+      {
+        name: 'vk',
+        width: '13',
+        height: '8',
+        href: '#'
+      },
+      {
+        name: 'twitter',
+        width: '15',
+        height: '10',
+        href: '#'
+      },
+      {
+        name: 'fb',
+        width: '5',
+        height: '11',
+        href: '#'
+      }
+    ]
+  }
+};
---
+++ markup/components/info-header/data/data.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+const data = {
+  infoHeader: {
+    title: 'Питомник<br>Мейн-Кунов',
+    location: 'Новосибирск',
+    description: 'Первый питомник с&nbsp;расширенным спектром&nbsp;услуг'
+  }
+};
